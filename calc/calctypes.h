74901618b3138b6668cb3b6cbb9587061892a253
vlitomsk <kvas.omsk@gmail.com>
2013-12-13 22:28:53 +0700

    Начальная версия калькулятора, еще не тестировал

--- calc/calctypes.h
+++ calc/calctypes.h
@@ -8,8 +8,8 @@ void number_to_str(Number n, char *s, int sz) {
 	snprintf(s, sz, "%f", n);
 }
 
-inline Number parse_number(const char *s) {
-	return (Number)atof(s);
+void p_number(Number n) {
+	printf("%f", n);
 }
 
 typedef enum {
@@ -29,12 +29,75 @@ typedef struct {
 	Status st;
 } EvalRes;
 
+void p_evalres(EvalRes r) {
+	if (r.st == SyntaxError)
+		printf("syntax error\n");
+	else if (r.st == DivByZero)
+		printf("division by zero\n");
+	else
+		p_number(r.res); 
+}
+
 inline EvalRes evdone(Number res) { // Evaluation OK
 	return (EvalRes) { res, EvalOk }; 
 }
 
 inline EvalRes everr(Status st) { // Evaluation error
 	return (EvalRes) { (Number)0, st };
+}
+
+inline Number parse_number(const char *s) {
+	return atof(s);
+}
+
+inline EvalRes number_add(Number a, Number b) {
+	return evdone(a + b);
+}
+
+inline EvalRes number_sub(Number a, Number b) {
+	return evdone(a - b);
+}
+
+inline EvalRes number_mul(Number a, Number b) {
+	return evdone(a * b);
+}
+
+inline EvalRes number_div(Number a, Number b) {
+	if (b == 0)
+		return everr(DivByZero);
+	return evdone(a / b);
+}
+
+inline EvalRes number_neg(Number a, Number stub) {
+	return evdone(-a);
+}
+
+EvalRes binop_res(EvalRes a, EvalRes b, EvalRes (*binop)(Number, Number)) {
+	if (a.st == SyntaxError || b.st == SyntaxError)
+		return everr(SyntaxError);
+	if (a.st == DivByZero || b.st == DivByZero)
+		return everr(DivByZero);
+	return binop(a.res, b.res);
+}
+
+EvalRes add_res(EvalRes a, EvalRes b) {
+	return binop_res(a, b, number_add);
+}
+
+EvalRes sub_res(EvalRes a, EvalRes b) {
+	return binop_res(a, b, number_sub);
+}
+
+EvalRes mul_res(EvalRes a, EvalRes b) {
+	return binop_res(a, b, number_mul);
+}
+
+EvalRes div_res(EvalRes a, EvalRes b) {
+	return binop_res(a, b, number_div);
+}
+
+EvalRes neg_res(EvalRes a) {
+	return binop_res(a, a, number_neg);
 }
 
 typedef enum {
@@ -68,7 +131,7 @@ inline Token symtok(TokenType t) {
 	return (Token) { t, 0, TokenOk };
 }
 
-void print_token(Token t) {
+void p_token(Token t) {
 	if (t.st == TokenOk) {
 		if (t.t == END)
 			printf("Token: END\n");
--- calc/main.c
+++ calc/main.c
@@ -4,22 +4,34 @@
 #include <ctype.h>
 #include "calctypes.h"
 
-/*
-	Expr:
-		Term+Expr
-		Term-Expr
-		Term
+/***********************/
+/* Разбиение на токены */
 
-	Term:
-		Prim*Term
-		Prim/Term
-		Prim
+#define MAX_TOKENS 1024
 
-	Prim:
-		(Expr)
-		Number
-		-Prim
-*/
+Token get_number_token(const char *s, char **ptr);
+/* ptr - указывает на место, с которого можно получать следующий токен */
+Token get_token(const char *s, char **ptr) {
+	printf("ch = %d\n", *s);
+	if (isspace(*s))
+		return get_token(s + 1, ptr);
+
+	if (*s == 0) {
+		*ptr = NULL;
+		return symtok(END);
+	}
+
+	if (isdigit(*s)) {
+		return get_number_token(s, ptr);
+	}
+
+	if (*s == '+' || *s == '-' || *s == '*' ||  *s == '/' || *s == '(' || *s == ')') {
+		*ptr = s + 1;
+		return symtok(*s);
+	}
+
+	return invtok();
+}
 
 Token get_number_token(const char *s, char **ptr) {
 	bool has_pt = false;
@@ -36,65 +48,148 @@ Token get_number_token(const char *s, char **ptr) {
 		} else 
 			break;
 	}
-
-	if (s[i] == 0) 
-		*ptr = NULL;
-	else
-		*ptr = s + i;
+	// printf("str: %d\n", s[i - 1]);
+	*ptr = s + i ;
 
 	return numtok(parse_number(s));
 }
 
-/* ptr - указывает на место, с которого можно получать следующий токен */
-Token get_token(const char *s, char **ptr) {
-	//printf("ch = %d\n", *s);
-	if (isspace(*s))
-		return get_token(s + 1, ptr);
+void tokenize(const char *s, Token *tokens, int sz, int *count) {
+	char *ptr = s;
+	*count = 0;
+	while (ptr && *count < sz)
+		tokens[*count++] = get_token(ptr, &ptr);	
+	// ДОАБВИТЬ ОБЯЗАТЕЛЬНО END!!
+}
 
-	if (*s == 0) {
-		*ptr = NULL;
-		return symtok(END);
+/*******************************/
+/* Вычисление expr, term, prim */
+
+/*** ГРАММАТИКА: ***/
+/* Expr:
+ *   Term + Expr
+ *	 Term - Expr
+ *	 Term
+ * 
+ * Term:
+ *   Prim * Term
+ *   Prim / Term
+ *   Prim
+ *
+ * Prim:
+ *   (Expr)
+ *   -Prim
+ *   Number
+ */
+
+EvalRes expr(const Token*, int, int);
+EvalRes term(const Token*, int, int);
+EvalRes prim(const Token*, int, int);
+
+EvalRes eval(const char *s) {
+	int n;
+	Token tokens[MAX_TOKENS];
+	tokenize(s, tokens, MAX_TOKENS, &n);
+	return expr(tokens, 0, n);
+}
+
+EvalRes expr(const Token *tok, int i, int n) {
+	p_token(*tok);
+	int j;
+
+	// разделяем по плюсу/минусу
+	for (j = i; j < n; ++j) {
+		if (tok[j].t == PLUS || tok[j].t == MINUS)
+			break;
 	}
 
-	if (isdigit(*s))
-		return get_number_token(s, ptr);
-
-	if (*s == '+' || *s == '-' || *s == '*' ||  *s == '/' || *s == '(' || *s == ')') {
-		*ptr = s + 1;
-		return symtok(*s);
+	if (j == n) {
+		puts("expr -> term");
+		return term(tok, i, n);
 	}
 
-	return invtok();
+	EvalRes _term = term(tok, i, j), 
+					_expr = expr(tok, j + 1, n);
+	if (tok[j].t == PLUS) 
+		return add_res(_term, _expr);
+	else if (tok[j].t == MINUS)
+		return sub_res(_term, _expr);
 }
 
-Number eval_expr() {
-	return 0; // stub
+EvalRes term(const Token *tok, int i, int n) {
+	p_token(*tok);
+	int j;
+
+	// разделяем по умножению/делению
+	for (j = i; j < n; ++j) {
+		if (tok[j].t == MUL || tok[j].t == DIV)
+			break;
+	}
+
+	if (j == n) {
+		puts("term -> prim");
+		return prim(tok, i, n);
+	}
+
+	EvalRes _prim = prim(tok, i, j),
+					_term = term(tok, j + 1, n);
+	if (tok[j].t == MUL)
+		return mul_res(_prim, _term);
+	else if (tok[j].t == DIV)
+		return mul_res(_prim, _term);
 }
 
-Number eval_term() {
-	return 0; // stub
+EvalRes prim(const Token *tok, int i, int n) {
+	p_token(*tok);
+
+	if (tok[i].t == MINUS) // unary minus
+		return neg_res(prim(tok, i + 1, n));
+
+	if (tok[i].t == LP) {
+		// выделяем выражение для передачи в expr
+		int j;
+		int depth = 0;
+		for (j = i; j < n; ++j) {
+			if (tok[j].t == LP)
+				++depth;
+			else if (tok[j].t == RP)
+				--depth;
+
+			if (depth == 0)
+				break;
+		}
+
+		if (depth != 0)
+			return everr(SyntaxError); // Parentheses mismatch
+
+		return expr(tok, i + 1, j);
+	} 
+
+	if (tok[i].t == NUM) 
+		return evdone(tok[i].val);
+
+	return everr(SyntaxError);
 }
 
-Number eval_prim() {
-	return 0; // stub
-}
+#define EXPR_LEN (MAX_TOKENS)
 
-#define EXPR_LEN 255
 int main() {
-	/*char s_expr[EXPR_LEN];
-	fgets(s_expr, EXPR_LEN, stdin);
-	Number res = eval_expr(s_expr);*/
+	// char s_expr[EXPR_LEN];
+	// fgets(s_expr, EXPR_LEN, stdin);
+	// EvalRes res = eval(s_expr);
+	// p_evalres(res);	
 
-	char *tmp = " \t-5 *   ((3.55  + 4 ) (7.55+2))(  ";
+	 //Проверка разделения на токены
+	char *tmp = "+";
 	char *ptr = tmp;
 
 	Token tok;
 	while (ptr) {
 		tok = get_token(ptr, &ptr);
-		print_token(tok);
+		p_token(tok);
 		if (tok.st == InvalidToken)
 			break;
 	}
-
+	
 	return 0;
 }
